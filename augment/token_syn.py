738e69562b685ab0788ec07d88ca2d9f7ee38e24
jiawei <b33b33o11@gmail.com>
2024-04-18 19:45:14 +0800

    fix: implement synonym identification w/ multiprocessing

--- augment/token_syn.py
+++ augment/token_syn.py
@@ -1,21 +1,19 @@
+import uuid
 import spacy
-from typing import List
+from typing import List, Dict, Set
 from nltk import pos_tag
 from nltk.corpus import wordnet
 from nltk.tokenize import word_tokenize
+from multiprocessing import Manager
+from multiprocessing.pool import Pool
 
-def synonym_substitution(
+def process_batch(
     inputs: List[str], 
-    ooc_words: List[str], 
-    n_generate: int = 20000
+    ooc_syns_dict: Dict[str, Set[str]], 
+    n_generate: int
 ) -> List[str]:
-    def flatten(xss): return [x for xs in xss for x in xs]
-
+    id = uuid.uuid4()
     nlp = spacy.load('en_core_web_md')
-
-    syns_dict = { 
-        word.lower() : flatten(wordnet.synonyms(word.lower())) for word in ooc_words 
-    }
 
     augmented_sentences = []
 
@@ -24,22 +22,53 @@ def synonym_substitution(
         words = word_tokenize(input_sentence)
         tagged_words = pos_tag(words)
 
-        for ooc_word, synonyms in syns_dict.items():
+        for ooc_word, synonyms in ooc_syns_dict.items():
             # process the ooc word to get its text and POS
             ooc_pos_tag = nlp(ooc_word)[0].tag_
 
             words_to_replace = set([word for word, pos in tagged_words if pos == ooc_pos_tag and word in synonyms])
 
             for syn in words_to_replace:
-                augmented_sentences.append(input_sentence.replace(syn, ooc_word))
+                augmented_sentences.append(input_sentence.replace(syn, ooc_word, 1))
                 
                 count = len(augmented_sentences)
                 # max of n_generate sentences will be created
                 if count >= n_generate: 
                     return augmented_sentences
-                elif count % 100 == 0:
-                    print(f"{count}: input_sentence")
+                elif count == 1 or count % 100 == 0:
+                    print(f"{id} {count}: {input_sentence}")
                     print(f"Replaced {syn} <-> {ooc_word}")
                     
                     
     return augmented_sentences
+
+def synonym_substitution(
+    inputs: List[str], 
+    ooc_words: List[str], 
+    n_generate: int=20000
+) -> List[str]:
+    batch_size = len(inputs) // 10
+    input_batches = [inputs[i:i+batch_size] for i in range(0, len(inputs), batch_size)]
+    num_threads = len(input_batches)
+    n_generate_per_batch = n_generate // num_threads
+
+    print("batch_size", batch_size)
+    print("num_threads", num_threads)
+    print("n_generate_per_batch", n_generate_per_batch)
+
+    def flatten(xss): return [x for xs in xss for x in xs]
+
+    syns_dict = { 
+        word.lower() : set(flatten(wordnet.synonyms(word.lower()))) for word in ooc_words 
+    }
+
+    with Manager() as manager:
+        augmented_sentences = manager.list()
+
+        with Pool() as pool:
+            args = [(batch, syns_dict, n_generate_per_batch) for batch in input_batches]
+            for result in pool.starmap_async(process_batch, args).get():
+                augmented_sentences.extend(result)
+
+        return list(augmented_sentences)
+    
